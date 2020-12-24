f6cac9150c3789c80b39686b32eddaec098f35bf
Stefano Pace <stefanopace01@gmail.com>
2020-12-24 12:54:46 +0100

    Day9 part 2 passed

--- lib/day9/day9.ex
+++ lib/day9/day9.ex
@@ -24,9 +24,26 @@ defmodule Day9 do
 	@doc """
 	## Examples
 		iex> Day9.part2
-		:error
+		3340942
 	"""
 	def part2 do
-		:error
+		numbers = 
+			Input.read(9)
+			|> Enum.map(fn strnum -> Integer.parse(strnum) |> elem(0) end)
+		
+		invalid = find_wrong(numbers)
+
+		find_weak_sequence(numbers, invalid, [])
+		|> Enum.min_max
+		|> (fn {min, max} -> min + max end).()
+
+	end
+
+	defp find_weak_sequence(numbers, invalid, current_sequence) do
+		case Enum.sum(current_sequence) do
+			^invalid -> current_sequence
+			sum when sum > invalid -> find_weak_sequence(tl(numbers), invalid, [])
+			_sum -> find_weak_sequence(numbers, invalid, [Enum.at(numbers, length(current_sequence)) | current_sequence])
+		end
 	end
 end
