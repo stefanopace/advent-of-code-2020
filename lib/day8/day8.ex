3e1d769dec690e130fee7d450e42886fa90eb1e1
Stefano Pace <stefanopace01@gmail.com>
2020-12-24 09:54:18 +0100

    passed day8 part 1

--- lib/day8/day8.ex
+++ lib/day8/day8.ex
@@ -2,10 +2,27 @@ defmodule Day8 do
 	@doc """
 	## Examples
 		iex> Day8.part1
-		:error
+		1600
 	"""
 	def part1 do
-		:todo
+		Input.read(8)
+		|> Enum.map(&decode_instruction/1)
+		|> find_loop(%{cur: 0, acc: 0})
+	end
+
+	defp find_loop(program, %{cur: cur, acc: acc}) do
+		case List.pop_at(program, cur) do
+			{{_, _, true}, _rest} -> acc
+			{{:jmp, val, _}, _rest} -> find_loop(List.replace_at(program, cur, {:jpm, val, true}), %{cur: cur + val, acc: acc})
+			{{:acc, val, _}, _rest} -> find_loop(List.replace_at(program, cur, {:acc, val, true}), %{cur: cur + 1, acc: acc + val})
+			{{:nop, val, _}, _rest} -> find_loop(List.replace_at(program, cur, {:nop, val, true}), %{cur: cur + 1, acc: acc})
+		end
+	end
+
+	defp decode_instruction(encoded) do
+		[cmd, num] = String.split(encoded)
+		{num, _rest} = Integer.parse(num)
+		{String.to_atom(cmd), num, false} 
 	end
 
 	@doc """
