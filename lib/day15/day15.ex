9cd58696ad2cc13bd5479093651c14441e303a9e
Stefano Pace <stefanopace01@gmail.com>
2020-12-28 22:06:07 +0100

    passed day15 part 2

--- lib/day15/day15.ex
+++ lib/day15/day15.ex
@@ -18,34 +18,49 @@ defmodule Day15 do
 			|> Map.new
 			|> (fn memory -> {memory, starting_numbers |> List.last, length(starting_numbers)} end).()
 		
-		Stream.iterate(starting_turns, &memory_game_turn/1)
-		|> Stream.take_while(fn {_, _, turn} -> turn <= 2020 end)
-		|> Enum.to_list
-		|> List.last
-		|> elem(1)
-	end
-
-	defp memory_game_turn({memory, last_spoken, turn}) do
-		case Map.has_key?(memory, last_spoken) do
-			false -> {
-				Map.put(memory, last_spoken, turn),
-				0,
-				turn + 1
-			}
-			true -> {
-				Map.put(memory, last_spoken, turn),
-				turn - Map.get(memory, last_spoken),
-				turn + 1
-			}
-		end
+		starting_turns
+		|> play_until(2020)
 	end
 
 	@doc """
 	## Examples
 		iex> Day15.part2
-		:error
+		323780
 	"""
 	def part2 do
-		:error
+		starting_numbers = 
+			Input.read(15)
+			|> List.first
+			|> String.split(",")
+			|> Enum.map(&(&1 |> Integer.parse |> elem(0)))
+			
+		starting_turns =
+			starting_numbers
+			|> Enum.with_index
+			|> Enum.map(fn {n, i} -> {n, i + 1} end)
+			|> Map.new
+			|> (fn memory -> {memory, starting_numbers |> List.last, length(starting_numbers)} end).()
+		
+		starting_turns
+		|> play_until(30000000)
+	end
+
+	defp play_until({memory, last_spoken, turn}, limit) do
+		if turn >= limit do
+			last_spoken
+		else
+			case Map.has_key?(memory, last_spoken) do
+				false -> play_until({
+					Map.put(memory, last_spoken, turn),
+					0,
+					turn + 1
+				}, limit)
+				true -> play_until({
+					Map.put(memory, last_spoken, turn),
+					turn - Map.get(memory, last_spoken),
+					turn + 1
+				}, limit)
+			end
+		end
 	end
 end
