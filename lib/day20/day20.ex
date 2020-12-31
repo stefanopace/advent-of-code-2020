1a1da28bf16b31839aaaaa8906ebd6acc5e9adf1
Stefano Pace <stefanopace01@gmail.com>
2020-12-31 19:14:02 +0100

    progress on day20

--- lib/day20/day20.ex
+++ lib/day20/day20.ex
@@ -8,80 +8,36 @@ defmodule Day20 do
 		# 21599955909991
 	"""
 	def part1(input) do
-		tiles = input |> parse_tiles
-
-		tiles
-		|> find_top_left_corner
-		|> compose_first_line
-		# |> compose_the_rest
-			
+		input 
+		|> parse_tiles
+		|> pick_one_tile
+		|> connect_other_tiles
 	end
 
-	defp compose_first_line({tiles, [row]}) do
-		row |> IO.inspect
-		last = List.last(row)
-		matched =
-			tiles
-			|> Enum.find_value(&(match_right(last, &1)))
-
-		case matched do
-			nil -> {tiles, [row]}
-			{matched_id, _} -> compose_first_line(
-				{
-					tiles
-					|> Enum.reject(fn {id, _} ->
-						id == matched_id
-					end), 
-					[row ++ [matched]]
-				}
-			)
-		end
+	defp connect_other_tiles({[], canvas}) do
+		canvas	
+	end
+	defp connect_other_tiles({tile_bag, canvas}) do
+		canvas
+		|> find_a_tile_missing_a_neighbor
+	
 	end
 
-	defp rotate({id, {[t,r,b,l], data}}) do
-		[first | _] = data
-		start = first |> String.graphemes |> Enum.map(fn _ -> [] end)
-		{
-			id,
-			{
-				[l,t,r,b],
-				data
-				|> Enum.reduce(start, fn row, partial -> 
-					Enum.zip(partial, row |> String.graphemes)
-					|> Enum.map(fn {l, r} -> [r | l] end)
+	defp find_a_tile_missing_a_neighbor(canvas) do
+		canvas
+		|> Enum.find_value(:work_done, fn tile = {{x, y}, data} ->
+			missing = 
+				[{0, -1}, {1, 0}, {0, 1}, {-1, 1}]
+				|> Enum.find(fn {dx, dy} -> 
+					not Map.has_key?(canvas, {x + dx, y + dy})
 				end)
-				|> Enum.map(&Enum.join/1)
-			}
-		}
+			
+			if missing, do: {tile, missing}, else: false
+		end)
 	end
 
-	defp flip_horizontal({id, {[t,r,b,l], data}}) do
-		{
-			id,
-			{
-				[t |> String.reverse, l |> String.reverse, b |> String.reverse, r |> String.reverse],
-				data
-				|> Enum.map(&String.reverse/1)
-			}
-		}
-	end
-
-	defp find_top_left_corner(tiles) do
-		corner = 
-			tiles
-			|> Enum.find(fn tile ->
-				[
-					Enum.find(tiles -- [tile], &(match_top(tile, &1))),
-					Enum.find(tiles -- [tile], &(match_right(tile, &1))),
-					Enum.find(tiles -- [tile], &(match_bottom(tile, &1))),
-					Enum.find(tiles -- [tile], &(match_left(tile, &1)))
-				]
-				|> Enum.filter(fn found -> found end)
-				|> Enum.count
-				|> Kernel.==(2)
-			end)
-		
-		{tiles -- [corner], [[corner]]}
+	defp pick_one_tile(_tile_bag = [first | rest]) do
+		{rest, %{{0, 0} => first}}
 	end
 
 	@doc """
@@ -99,65 +55,21 @@ defmodule Day20 do
 		|> Enum.map(&parse_tile/1)
 	end
 
-	defp parse_tile([encoded_id | data]) do
+	defp parse_tile([encoded_id | encoded_data]) do
 		id = Regex.run(~r/Tile (\d+):/, encoded_id) |> Enum.at(1) |> Integer.parse |> elem(0)
 
-		top = List.first(data)
-		right = Enum.map(data, &String.last/1) |> Enum.join
-		bottom = List.last(data) |> String.reverse
-		left = Enum.map(data, &String.first/1) |> Enum.join |> String.reverse
+		data =
+			encoded_data
+			|> Enum.with_index
+			|> Enum.map(fn {row, y} ->
+				row
+				|> String.graphemes
+				|> Enum.with_index
+				|> Enum.map(fn {pixel, x} -> {{x, y}, pixel} end)
+			end)
+			|> List.flatten
+			|> Map.new
 
-		{id, {[top, right, bottom, left], data}}
-	end
-
-	defp rotations_of(tile) do
-		r90 = rotate(tile)
-		r180 = rotate(r90)
-		r270 = rotate(r180)
-		flipped = flip_horizontal(tile)
-		fr90 = rotate(flipped)
-		fr180 = rotate(fr90)
-		fr270 = rotate(fr180)
-		[tile, r90, r180, r270, flipped, fr90, fr180, fr270]
-	end
-
-	defp match_right(tile1, tile2) do
-		{_, {[_, right, _, _], _}} = tile1
-
-		rotations_of(tile2)
-		|> Enum.find(false, fn rotation ->
-			{_, {[_, _, _, left], _}} = rotation
-			right == String.reverse(left)
-		end)
-	end
-
-	defp match_top(tile1, tile2) do
-		{_, {[top, _, _, _], _}} = tile1
-
-		rotations_of(tile2)
-		|> Enum.find(false, fn rotation ->
-			{_, {[_, _, bottom, _], _}} = rotation
-			top == String.reverse(bottom)
-		end)
-	end
-
-	defp match_bottom(tile1, tile2) do
-		{_, {[_, _, bottom, _], _}} = tile1
-
-		rotations_of(tile2)
-		|> Enum.find(false, fn rotation ->
-			{_, {[top, _, _, _], _}} = rotation
-			bottom == String.reverse(top)
-		end)
-	end
-
-	defp match_left(tile1, tile2) do
-		{_, {[_, _, _, left], _}} = tile1
-
-		rotations_of(tile2)
-		|> Enum.find(false, fn rotation ->
-			{_, {[_, right, _, _], _}} = rotation
-			left == String.reverse(right)
-		end)
+		{id, data}
 	end
 end
