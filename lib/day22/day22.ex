af186e45caaa8a7a50dc9e0f0e1456df6206e2dc
Stefano Pace <stefanopace01@gmail.com>
2021-01-04 12:51:08 +0100

    passed day22 part2

--- lib/day22/day22.ex
+++ lib/day22/day22.ex
@@ -26,8 +26,55 @@ defmodule Day22 do
 			input
 			|> parse_decks
 
-		play(player1_deck, player2_deck)
+		combat(player1_deck, player2_deck)
 		|> compute_score
+	end
+
+	@doc """
+	## Examples
+		iex> [
+		...> 	"Player 1:",
+		...> 	"9",
+		...> 	"2",
+		...> 	"6",
+		...> 	"3",
+		...> 	"1",
+		...> 	"",
+		...> 	"Player 2:",
+		...> 	"5",
+		...> 	"8",
+		...> 	"4",
+		...> 	"7",
+		...> 	"10"
+		...> ] |> Day22.part2
+		291
+		
+		# test too slow
+		# iex> Input.read(22) |> Day22.part2
+		# 32317
+	"""
+	def part2(input) do
+		[player1_deck, player2_deck] =
+			input
+			|> parse_decks
+
+		recursive_combat(player1_deck, player2_deck, [])
+		|> compute_score
+	end
+
+	defp recursive_combat(deck1, [], _), do: {:player1, deck1}
+	defp recursive_combat([], deck2, _), do: {:player2, deck2}
+	defp recursive_combat(deck1 = [card1 | rest1], deck2 = [card2 | rest2], previous_turns) do
+		cond do
+			previous_turns |> Enum.member?({deck1, deck2}) -> {:player1, deck1}
+			length(rest1) >= card1 and length(rest2) >= card2 -> 
+				case recursive_combat(Enum.take(rest1, card1), Enum.take(rest2, card2), []) do
+					{:player1, _} -> recursive_combat(rest1 ++ [card1, card2], rest2, [{deck1, deck2} | previous_turns])
+					{:player2, _} -> recursive_combat(rest1, rest2 ++ [card2, card1], [{deck1, deck2} | previous_turns])
+				end
+			card1 > card2 -> recursive_combat(rest1 ++ [card1, card2], rest2, [{deck1, deck2} | previous_turns])
+			card2 > card1 -> recursive_combat(rest1, rest2 ++ [card2, card1], [{deck1, deck2} | previous_turns])
+		end
 	end
 
 	defp compute_score({_winning_player, deck}) do
@@ -38,13 +85,13 @@ defmodule Day22 do
 		|> Enum.sum
 	end
 
-	defp play(deck1, []), do: {:player1, deck1}
-	defp play([], deck2), do: {:player2, deck2}
-	defp play([card1 | deck1], [card2 | deck2]) when card1 > card2 do
-		play(deck1 ++ [card1, card2], deck2)
+	defp combat(deck1, []), do: {:player1, deck1}
+	defp combat([], deck2), do: {:player2, deck2}
+	defp combat([card1 | deck1], [card2 | deck2]) when card1 > card2 do
+		combat(deck1 ++ [card1, card2], deck2)
 	end
-	defp play([card1 | deck1], [card2 | deck2]) when card1 < card2 do
-		play(deck1, deck2 ++ [card2, card1])
+	defp combat([card1 | deck1], [card2 | deck2]) when card1 < card2 do
+		combat(deck1, deck2 ++ [card2, card1])
 	end
 
 	defp parse_decks(lines) do
@@ -57,12 +104,4 @@ defmodule Day22 do
 		end)
 	end
 
-	@doc """
-	## Examples
-		iex> Input.read(22) |> Day22.part2
-		:result
-	"""
-	def part2(_input) do
-		:result
-	end
 end
