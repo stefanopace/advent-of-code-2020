1f5e9bd5ec6e02a2ab18a02911a2312c2e9b03dd
Stefano Pace <stefanopace01@gmail.com>
2021-01-05 14:18:21 +0100

    decoded directions input

--- lib/day24/day24.ex
+++ lib/day24/day24.ex
@@ -1,11 +1,50 @@
 defmodule Day24 do
 	@doc """
 	## Examples
-		iex> Input.read(24) |> Day24.part1
-		:result
+		iex> [
+		...>	"sesenwnenenewseeswwswswwnenewsewsw",
+		...>	"neeenesenwnwwswnenewnwwsewnenwseswesw",
+		...>	"seswneswswsenwwnwse",
+		...>	"nwnwneseeswswnenewneswwnewseswneseene",
+		...>	"swweswneswnenwsewnwneneseenw",
+		...>	"eesenwseswswnenwswnwnwsewwnwsene",
+		...>	"sewnenenenesenwsewnenwwwse",
+		...>	"wenwwweseeeweswwwnwwe",
+		...>	"wsweesenenewnwwnwsenewsenwwsesesenwne",
+		...>	"neeswseenwwswnwswswnw",
+		...>	"nenwswwsewswnenenewsenwsenwnesesenew",
+		...>	"enewnwewneswsewnwswenweswnenwsenwsw",
+		...>	"sweneswneswneneenwnewenewwneswswnese",
+		...>	"swwesenesewenwneswnwwneseswwne",
+		...>	"enesenwswwswneneswsenwnewswseenwsese",
+		...>	"wnwnesenesenenwwnenwsewesewsesesew",
+		...>	"nenewswnwewswnenesenwnesewesw",
+		...>	"eneswnwswnwsenenwnwnwwseeswneewsenese",
+		...>	"neswnwewnwnwseenwseesewsenwsweewe",
+		...>	"wseweeenwnesenwwwswnew"
+		...> ] |> Day24.part1
+		10
+
+		# iex> Input.read(24) |> Day24.part1
+		# :result
 	"""
 	def part1(input) do
-		:result
+		input
+		|> Enum.map(&String.graphemes/1)
+		|> Enum.map(&decode_directions/1)
+	end
+
+	defp decode_directions(encoded), do: decode_directions(encoded, [])
+	defp decode_directions(encoded, decoded) do
+		case encoded do
+			[] -> decoded |> Enum.reverse
+			["s", "e" | rest] -> decode_directions(rest, [:south_east | decoded])
+			["s", "w" | rest] -> decode_directions(rest, [:south_west | decoded])
+			["n", "w" | rest] -> decode_directions(rest, [:north_west | decoded])
+			["n", "e" | rest] -> decode_directions(rest, [:north_east | decoded])
+			["e" | rest] -> decode_directions(rest, [:east | decoded])
+			["w" | rest] -> decode_directions(rest, [:west | decoded])
+		end
 	end
 
 	@doc """
