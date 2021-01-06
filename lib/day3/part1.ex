c1129c61ce1a1f49295d4694658f8316c183bd79
Stefano Pace <stefanopace01@gmail.com>
2021-01-06 21:11:44 +0100

    refactored day3 part1

--- lib/day3/part1.ex
+++ lib/day3/part1.ex
@@ -1,4 +1,5 @@
 defmodule Day3.Part1 do
+
 	@doc """
 	## Examples
 		iex> Input.read("./lib/day3/input") |> Day3.Part1.solve
@@ -8,19 +9,19 @@ defmodule Day3.Part1 do
 		count_trees(input, 3, 1)
 	end
 
-	def count_trees(input, horizontal_move, vertical_move) do
+	def count_trees(input, horizontal_pace, vertical_pace) do
 		input
-		|> Enum.take_every(vertical_move)
-		|> Enum.map_reduce(0,
-			fn (current, cursor) ->
-				{	
-					String.at(current, cursor),
-					rem(cursor + horizontal_move, String.length(current))
-				}
-			end
-		)
+		|> Enum.take_every(vertical_pace)
+		|> Enum.map_reduce(0, &(move_horizontally(&1, &2, horizontal_pace)))
 		|> elem(0)
 		|> Enum.count(&(&1 == "#"))
+	end
+
+	defp move_horizontally(row, position, pace) do
+		{	
+			row |> String.at(position),
+			position + pace |> Integer.mod(String.length(row))
+		}
 	end
 
 end
