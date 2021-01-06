1225f47a008c09005e69532e45da70ebe4b8786d
Stefano Pace <stefanopace01@gmail.com>
2021-01-06 18:52:40 +0100

    refactored day1

--- lib/day1/day1.ex
+++ lib/day1/day1.ex
@@ -4,30 +4,54 @@ defmodule Day1 do
 		iex> ["1721", "979", "366", "299", "675", "1456"] |> Day1.part1
 		514579
 
-		iex> Input.read(1) |> Day1.part1
+		iex> Input.read("./lib/day1/input") |> Day1.part1
 		776064
 	"""
 	def part1(input) do
-		{x, y} =
-			input
-			|> Stream.map(&String.to_integer/1)
-			|> (&(for x <- &1, y <- &1, do: {x, y})).()
-			|> Enum.find(fn {x, y} -> x + y == 2020 end)
-		x * y
+		input
+		|> to_integers
+		|> combine_into_pairs
+		|> Enum.find_value(fn 
+			{a, b} when a + b == 2020 -> a * b
+			_________________________ -> false
+		end)
 	end
 
 	@doc """
 	## Examples
 		iex> ["1721", "979", "366", "299", "675", "1456"] |> Day1.part1
 		514579
+
+		iex> Input.read("./lib/day1/input") |> Day1.part2
+		6964490
 	"""
-	def part2(input) do
-		numbers = 
-			input
-			|> Enum.map(&Integer.parse/1)
-			|> Enum.map(fn {parsed, _rest} -> parsed end)
-		trines = for x <- numbers, y <- numbers, z <- numbers, do: {x, y, z}
-		{x, y, z} = Enum.find(trines, fn {x, y, z} -> x + y + z == 2020 end)
-		x * y * z
+	def part2(input) do 
+		input
+		|> to_integers
+		|> combine_into_triples
+		|> Enum.find_value(fn 
+			{a, b, c} when a + b + c == 2020 -> a * b * c
+			________________________________ -> false
+		end)
+	end
+
+	defp to_integers(numbers), do: numbers |> Stream.map(&String.to_integer/1)
+
+	defp combine_into_pairs(numbers) do
+		Stream.flat_map(numbers, fn a ->
+			Stream.flat_map(numbers, fn b -> 
+				[{a, b}] 
+			end) 
+		end)
+	end
+
+	defp combine_into_triples(numbers) do
+		Stream.flat_map(numbers, fn a -> 
+			Stream.flat_map(numbers, fn b -> 
+				Stream.flat_map(numbers, fn c -> 
+					[{a, b, c}] 
+				end) 
+			end) 
+		end)
 	end
 end
