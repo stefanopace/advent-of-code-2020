d56bccc0fbcef963a4a0de135a86c5dabdbb0bb8
Stefano Pace <stefanopace01@gmail.com>
2021-01-06 22:47:02 +0100

    refactored day6 part1

--- lib/day6/part1.ex
+++ lib/day6/part1.ex
@@ -8,9 +8,13 @@ defmodule Day6.Part1 do
 	def solve(input) do
 		input
 		|> Input.split_on_blank_lines
-		|> Enum.map(fn group -> Enum.flat_map(group, &String.graphemes/1) end)
-		|> Enum.map(&MapSet.new/1)
-		|> Enum.map(&MapSet.size/1)
+		|> Enum.map(fn group -> 
+			group
+			|> Enum.map(&String.graphemes/1)
+			|> Enum.map(&MapSet.new/1)
+			|> Enum.reduce(&MapSet.union/2)
+			|> MapSet.size
+		end)
 		|> Enum.sum
 	end
 
