d3d1e3ca0266d5166bf506a057adee5b8918059f
Stefano Pace <stefanopace01@gmail.com>
2021-01-06 21:38:41 +0100

    refactored day4

--- lib/day4/part1.ex
+++ lib/day4/part1.ex
@@ -2,28 +2,29 @@ defmodule Day4.Part1 do
 
 	@doc """
 	## Examples
-		iex> Input.read(4) |> Day4.Part1.solve
+		iex> Input.read("./lib/day4/input") |> Day4.Part1.solve
 		230
 	"""
 	def solve(input) do
 		input
 		|> parse_documents
-		|> Enum.filter(&required_fields_are_present/1)
-		|> Enum.count
+		|> Enum.count(&are_required_fields_present?/1)
 	end
 
 	def parse_documents(lines) do
 		lines
 		|> Input.split_on_blank_lines
 		|> Enum.map(fn doc -> Enum.flat_map(doc, fn record -> String.split(record, " ") end) end)
-		|> Enum.map(fn doc -> Enum.map(doc, fn record -> 
-			[_all, k, v] = Regex.run(~r"(.*):(.*)", record)
-			{String.to_atom(k), v}
-			end )
-		end )
+		|> Enum.map(fn doc -> Enum.map(doc, 
+			fn record ->
+				record
+				|> Input.regex_match(~r"(.*):(.*)")
+				|> (fn {k, v} -> {String.to_atom(k), v} end).()
+			end)
+		end)
 	end
 
-	def required_fields_are_present(doc) do
+	def are_required_fields_present?(doc) do
 		[:byr, :iyr, :eyr, :hgt, :hcl, :ecl, :pid]
 		|> Enum.all?(fn field -> Keyword.has_key?(doc, field) end)
 	end
--- lib/day4/part2.ex
+++ lib/day4/part2.ex
@@ -4,46 +4,34 @@ defmodule Day4.Part2 do
 
 	@doc """
 	## Examples
-		iex> Input.read(4) |> Day4.Part2.solve
+		iex> Input.read("./lib/day4/input") |> Day4.Part2.solve
 		156
 	"""
 	def solve(input) do
 		input
 		|> Part1.parse_documents
-		|> Enum.filter(&Part1.required_fields_are_present/1)
-		|> Enum.filter(&fields_are_valids/1)
-		|> Enum.count
+		|> Enum.filter(&Part1.are_required_fields_present?/1)
+		|> Enum.count(&are_all_fields_valid?/1)
 	end
 
-	defp fields_are_valids(doc) do
-		Enum.all?(doc, &field_is_valid/1)
-	end
+	defp are_all_fields_valid?(doc), do: doc |> Enum.all?(&is_field_valid?/1)
 
-	defp between(num, min, max) do
-		{num, _rest} = Integer.parse(num, 10)
-		min <= num and num <= max
-	end
-
-	defp field_is_valid({:byr, val}), do: between(val, 1920, 2002)
-	defp field_is_valid({:iyr, val}), do: between(val, 2010, 2020)
-	defp field_is_valid({:eyr, val}), do: between(val, 2020, 2030)
-	defp field_is_valid({:hgt, val}) do
+	defp is_field_valid?({:byr, val}), do: val |> String.to_integer |> between(1920, 2002)
+	defp is_field_valid?({:iyr, val}), do: val |> String.to_integer |> between(2010, 2020)
+	defp is_field_valid?({:eyr, val}), do: val |> String.to_integer |> between(2020, 2030)
+	defp is_field_valid?({:hcl, val}), do: Regex.match?(~r"^#[0-9a-f]{6}$", val)
+	defp is_field_valid?({:ecl, val}), do: Enum.member?(["amb","blu","brn","gry","grn","hzl","oth"], val)
+	defp is_field_valid?({:pid, val}), do: Regex.match?(~r"^[0-9]{9}$", val)
+	defp is_field_valid?({:cid, _val}), do: true
+	defp is_field_valid?({:hgt, val}) do
 		{val, unit} = Integer.parse(val, 10)
 		case unit do
-			"cm" -> 150 <= val and val <= 193
-			"in" -> 59 <= val and val <= 76
+			"cm" -> val |> between(150, 193)
+			"in" -> val |> between(59, 76)
 			_ -> false
 		end
 	end
-	defp field_is_valid({:hcl, val}) do
-		Regex.match?(~r"^#[0-9a-f]{6}$", val)
-	end
-	defp field_is_valid({:ecl, val}) do
-		Enum.member?(["amb","blu","brn","gry","grn","hzl","oth"], val)
-	end
-	defp field_is_valid({:pid, val}) do
-		Regex.match?(~r"^[0-9]{9}$", val)
-	end
-	defp field_is_valid({:cid, _val}), do: true
+
+	defp between(num, min, max), do: min <= num and num <= max
 
 end
