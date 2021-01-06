5d10defb04467a59479b294a40680087f08916c0
Stefano Pace <stefanopace01@gmail.com>
2021-01-06 21:52:59 +0100

    extracted function

--- lib/day5/part1.ex
+++ lib/day5/part1.ex
@@ -7,34 +7,28 @@ defmodule Day5.Part1 do
 	"""
 	def solve(input) do
 		input
-		|> Enum.map(&decode/1)
+		|> Enum.map(&decode_ticket/1)
 		|> Enum.max_by(fn {_row, _col, id, _code} -> id end)
 		|> elem(2)
 	end
 
-	def decode(code) do
+	def decode_ticket(code) do
 		{row_encoded, col_encoded} = String.split_at(code, 7)
-		row = 
-			row_encoded
-			|> String.codepoints
-			|> Enum.reverse
-			|> Enum.with_index
-			|> Enum.map(fn {code, index} -> {code, :math.pow(2,index)} end)
-			|> Enum.filter(fn {code, _pow} -> code == "B" end)
-			|> Enum.map(fn {_code, pow} -> floor(pow) end)
-			|> Enum.sum
-
-		col =
-			col_encoded
-			|> String.codepoints
-			|> Enum.reverse
-			|> Enum.with_index
-			|> Enum.map(fn {code, index} -> {code, :math.pow(2,index)} end)
-			|> Enum.filter(fn {code, _pow} -> code == "R" end)
-			|> Enum.map(fn {_code, pow} -> floor(pow) end)
-			|> Enum.sum
+		row = row_encoded |> sum_powers_of_2_matching("B")
+		col = col_encoded |> sum_powers_of_2_matching("R")
 		
 		{row, col, (row * 8) + col, code}
+	end
+
+	defp sum_powers_of_2_matching(code, char) do
+		code
+		|> String.graphemes
+		|> Enum.reverse
+		|> Enum.with_index
+		|> Enum.map(fn {char, index} -> {char, :math.pow(2,index)} end)
+		|> Enum.filter(fn {code, _pow} -> code == char end)
+		|> Enum.map(fn {_code, pow} -> floor(pow) end)
+		|> Enum.sum
 	end
 
 end
--- lib/day5/part2.ex
+++ lib/day5/part2.ex
@@ -9,7 +9,7 @@ defmodule Day5.Part2 do
 	"""
 	def solve(input) do
 		input
-		|> Enum.map(&Part1.decode/1)
+		|> Enum.map(&Part1.decode_ticket/1)
 		|> Enum.sort_by(fn {_row, _col, id, _code} -> id end)
 		|> Enum.chunk_every(2, 1, :discard)
 		|> Enum.find(fn [{_, _, l, _}, {_, _, r, _}] -> r - l != 1 end)
