2fa66948907f8e1510b113853ac7530185c465ff
Stefano Pace <stefanopace01@gmail.com>
2021-01-07 00:38:19 +0100

    refactored day4 part2

--- lib/day4/part2.ex
+++ lib/day4/part2.ex
@@ -16,9 +16,9 @@ defmodule Day4.Part2 do
 
 	defp are_all_fields_valid?(doc), do: doc |> Enum.all?(&is_field_valid?/1)
 
-	defp is_field_valid?({:byr, val}), do: val |> String.to_integer |> between(1920, 2002)
-	defp is_field_valid?({:iyr, val}), do: val |> String.to_integer |> between(2010, 2020)
-	defp is_field_valid?({:eyr, val}), do: val |> String.to_integer |> between(2020, 2030)
+	defp is_field_valid?({:byr, val}), do: val |> String.to_integer |> between?(1920, 2002)
+	defp is_field_valid?({:iyr, val}), do: val |> String.to_integer |> between?(2010, 2020)
+	defp is_field_valid?({:eyr, val}), do: val |> String.to_integer |> between?(2020, 2030)
 	defp is_field_valid?({:hcl, val}), do: Regex.match?(~r"^#[0-9a-f]{6}$", val)
 	defp is_field_valid?({:ecl, val}), do: Enum.member?(["amb","blu","brn","gry","grn","hzl","oth"], val)
 	defp is_field_valid?({:pid, val}), do: Regex.match?(~r"^[0-9]{9}$", val)
@@ -26,12 +26,12 @@ defmodule Day4.Part2 do
 	defp is_field_valid?({:hgt, val}) do
 		{val, unit} = Integer.parse(val, 10)
 		case unit do
-			"cm" -> val |> between(150, 193)
-			"in" -> val |> between(59, 76)
-			_ -> false
+			"cm" -> val |> between?(150, 193)
+			"in" -> val |> between?(59, 76)
+			____ -> false
 		end
 	end
 
-	defp between(num, min, max), do: min <= num and num <= max
+	defp between?(num, min, max), do: min <= num and num <= max
 
 end
