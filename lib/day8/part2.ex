67b21defa0edbc5d026651aa98c330f87e7156ce
Stefano Pace <stefanopace01@gmail.com>
2021-01-08 09:53:58 +0100

    refactoring day8 part2

--- lib/day8/part2.ex
+++ lib/day8/part2.ex
@@ -8,40 +8,50 @@ defmodule Day8.Part2 do
 		1543
 	"""
 	def solve(input) do
-		input
-		|> Enum.map(&Part1.decode_instruction/1)
-		|> traverse(%{cur: 0, acc: 0, fixed: false})
+		%{
+			instructions: input |> Enum.map(&Part1.decode_instruction/1),
+			cursor: 0,
+			accumulator: 0,
+			already_swapped: false
+		}
+		|> traverse
 	end
 
-	defp traverse(program, %{cur: cur, acc: acc, fixed: fixed}) do
+	defp traverse(%{instructions: program, cursor: cur, accumulator: acc, already_swapped: already_swapped}) do
 		case length(program) do
 			^cur -> acc
 			_ -> case program |> Enum.at(cur) do
 					%{visited: true} -> :wrong_path
 					%{instruction: cmd, value: val} ->
-						traverse(Part1.mark_as_visited(program, cur), next_state(cmd, val, cur, acc, fixed))
+						traverse(
+							
+								next_state(cmd, val, cur, acc, already_swapped, Part1.mark_as_visited(program, cur))
+							
+						)
 						|> case do
 							result when cmd == :acc -> result
-							:wrong_path when not fixed -> traverse(Part1.mark_as_visited(program, cur), swap_instructions(cmd, val, cur, acc, true))
+							:wrong_path when not already_swapped -> traverse(swap_instructions(cmd, val, cur, acc, true, Part1.mark_as_visited(program, cur)))
 							result -> result
 						end
 				end
 		end
 	end
 
-	defp next_state(cmd, val, cur, acc, fixed) do
+	defp next_state(cmd, val, cur, acc, already_swapped, instructions) do
 		%{
-			cur: cur + (if cmd == :jmp, do: val, else: 1), 
-			acc: acc + (if cmd == :acc, do: val, else: 0),
-			fixed: fixed
+			cursor: cur + (if cmd == :jmp, do: val, else: 1), 
+			accumulator: acc + (if cmd == :acc, do: val, else: 0),
+			already_swapped: already_swapped,
+			instructions: instructions
 		}
 	end
 
-	defp swap_instructions(cmd, val, cur, acc, fixed) do
+	defp swap_instructions(cmd, val, cur, acc, already_swapped, instructions) do
 		%{
-			cur: cur + (if cmd == :jmp, do: 1, else: val), 
-			acc: acc, 
-			fixed: fixed
+			cursor: cur + (if cmd == :jmp, do: 1, else: val), 
+			accumulator: acc, 
+			already_swapped: already_swapped,
+			instructions: instructions
 		}
 	end
 
